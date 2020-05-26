f4ee060a4aa4b5aa2a90b438bb93ecf0d0db6979
Student Name <rexsutton2@gmail.cogit>
2020-05-26 16:00:16 -0500

    school day ended finish last two pts of exercise tomorrow

---
+++ update_exercises.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+SELECT 'all albums' AS 'all albums';
+SELECT name FROM albums;
+SELECT 'before 1980' AS 'All albums';
+SELECT name FROM albums WHERE release_date BETWEEN 1800 AND 1980;
+SELECT 'BY MJ' AS 'ALL ALBUMS';
+SELECT name FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET sales = (sales * 10);
+WHERE sales = ;
